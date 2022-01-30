cd3e0a7fff2ea580c0c0125e6703dab8f9439cef
Klex666 <57945831+Klex666@users.noreply.github.com>
2022-01-30 20:47:05 +0400

    add timer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -34,4 +36,61 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         } 
     });
+
+    //Timer
+
+   const deadline = "2022-02-31";
+
+
+   function getTimeRemaining(endtime) {
+     const t = Date.parse(endtime) - Date.parse(new Date());
+
+     const days = Math.floor( (t/(1000*60*60*24)) );
+     const seconds = Math.floor( (t/1000) % 60 );
+     const minutes = Math.floor( (t/1000/60) % 60 );
+     const hours = Math.floor( (t/(1000*60*60) % 24) );
+
+     return { 
+        'total': t,
+        'days': days,
+        'seconds': seconds,
+        'minutes': minutes,
+        'hours': hours
+     };
+   }
+
+   function correct(num) {
+       if (num >= 0 && num < 10) {
+           return '0' + num;
+       } else {
+           return num;
+       }
+   }
+
+   function setClock(selector, endtime) {
+       const timer = document.querySelector(selector),
+             days = timer.querySelector('#days'),
+             seconds = timer.querySelector('#seconds'),
+             minutes = timer.querySelector('#minutes'),
+             hours = timer.querySelector('#hours'),
+             timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+     function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = correct(t.days);
+        hours.innerHTML = correct(t.hours);
+        minutes.innerHTML = correct(t.minutes);
+        seconds.innerHTML = correct(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+     }        
+   }
+
+   setClock('.timer', deadline);
+
 }); 
